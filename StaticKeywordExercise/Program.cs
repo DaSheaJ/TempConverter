3e55d62a6d7cbe4899e78c868b32a6e50d67766d
DaSheaJ <dashea.johnson.1@gmail.com>
2021-06-27 22:53:35 -0500

    initial commit

---
+++ StaticKeywordExercise/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace StaticKeywordExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var celsius = TempConverter.FahrenheitToCelsius(68);
+            var fahrenheit = TempConverter.CelciusToFahrenheit(20);
+
+            Console.WriteLine($"celsius {celsius}");
+            Console.WriteLine($"fahrenhiet {fahrenheit}");
+        }
+    }
+}
